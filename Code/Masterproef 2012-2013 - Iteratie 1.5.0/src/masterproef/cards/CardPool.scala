9fcffafcd97755307c34de04636158937fff782b
Kenneth Saey <kenneth@ugent>
2013-02-22 11:17:43 +0100

    Enabled all creatures

--- Code/Masterproef 2012-2013 - Iteratie 1.5.0/src/masterproef/cards/CardPool.scala
+++ Code/Masterproef 2012-2013 - Iteratie 1.5.0/src/masterproef/cards/CardPool.scala
@@ -25,85 +25,85 @@ object CardPool {
 			with_default_damage 1
 			with_default_health 1
 			has_ability2 Absorb(4),
-//		new Creature called "Bloodlord of Vaasgoth"
-//			with_default_damage 3
-//			with_default_health 3
-//			has_ability Flying,
-//		new Creature called "Bloodrage Vampire"
-//			with_default_damage 3
-//			with_default_health 1
-//			has_ability Lifelink,
+		new Creature called "Bloodlord of Vaasgoth"
+			with_default_damage 3
+			with_default_health 3
+			has_ability Flying,
+		new Creature called "Bloodrage Vampire"
+			with_default_damage 3
+			with_default_health 1
+			has_ability Lifelink,
 		new Creature called "Cemetery Reaper"
 			with_default_damage 2
 			with_default_health 2
 			has_ability Deathtouch,
-//		new Creature called "Child of Night"
-//			with_default_damage 2
-//			with_default_health 1
-//			has_ability Defender,
-//		new Creature called "Devouring Swarm"
-//			with_default_damage 2
-//			with_default_health 1
-//			has_ability Flying,
-//		new Creature called "Drifting Shade"
-//			with_default_damage 1
-//			with_default_health 1
-//			has_ability Flying
-//			has_ability Shadow,
-//		new Creature called "Duskhunter Bat"
-//			with_default_damage 4
-//			with_default_health 2
-//			has_ability Flying
-//			has_ability Trample,
-//		new Creature called "Grave Titan"
-//			with_default_damage 6
-//			with_default_health 6
-//			has_ability Reach,
-//		new Creature called "Gravedigger"
-//			with_default_damage 2
-//			with_default_health 2
-//			has_ability Horsemanship,
-//		new Creature called "Onyx Mage"
-//			with_default_damage 2
-//			with_default_health 1
-//			has_ability Reach,
-//		new Creature called "Reassembling Skeleton"
-//			with_default_damage 1
-//			with_default_health 1
-//			has_ability Indestructible,
-//		new Creature called "Royal Assassin"
-//			with_default_damage 1
-//			with_default_health 1
-//			has_ability Shadow,
+		new Creature called "Child of Night"
+			with_default_damage 2
+			with_default_health 1
+			has_ability Defender,
+		new Creature called "Devouring Swarm"
+			with_default_damage 2
+			with_default_health 1
+			has_ability Flying,
+		new Creature called "Drifting Shade"
+			with_default_damage 1
+			with_default_health 1
+			has_ability Flying
+			has_ability Shadow,
+		new Creature called "Duskhunter Bat"
+			with_default_damage 4
+			with_default_health 2
+			has_ability Flying
+			has_ability Trample,
+		new Creature called "Grave Titan"
+			with_default_damage 6
+			with_default_health 6
+			has_ability Reach,
+		new Creature called "Gravedigger"
+			with_default_damage 2
+			with_default_health 2
+			has_ability Horsemanship,
+		new Creature called "Onyx Mage"
+			with_default_damage 2
+			with_default_health 1
+			has_ability Reach,
+		new Creature called "Reassembling Skeleton"
+			with_default_damage 1
+			with_default_health 1
+			has_ability Indestructible,
+		new Creature called "Royal Assassin"
+			with_default_damage 1
+			with_default_health 1
+			has_ability Shadow,
 		new Creature called "Rune-Scarred Demon"
 			with_default_damage 6
 			with_default_health 6,
-//		new Creature called "Sengir Vampire"
-//			with_default_damage 4
-//			with_default_health 4
-//			has_ability Lifelink,
-//		new Creature called "Sutured Ghoul"
-//			with_default_damage 3
-//			with_default_health 3
-//			has_ability Trample,
-//		new Creature called "Tormented Soul"
-//			with_default_damage 1
-//			with_default_health 1
-//			has_ability Unblockable,
-//		new Creature called "Vampire Outcasts"
-//			with_default_damage 2
-//			with_default_health 2
-//			has_ability Lifelink,
-//		new Creature called "Vengeful Pharaoh"
-//			with_default_damage 5
-//			with_default_health 4,
+		new Creature called "Sengir Vampire"
+			with_default_damage 4
+			with_default_health 4
+			has_ability Lifelink,
+		new Creature called "Sutured Ghoul"
+			with_default_damage 3
+			with_default_health 3
+			has_ability Trample,
+		new Creature called "Tormented Soul"
+			with_default_damage 1
+			with_default_health 1
+			has_ability Unblockable,
+		new Creature called "Vampire Outcasts"
+			with_default_damage 2
+			with_default_health 2
+			has_ability Lifelink,
+		new Creature called "Vengeful Pharaoh"
+			with_default_damage 5
+			with_default_health 4,
 		new Creature called "Warpath Ghoul"
 			with_default_damage 3
-			with_default_health 2//,
-//		new Creature called "Zombie Goliath"
-//			with_default_damage 4
-//			with_default_health 4
-//			has_ability Reach
+			with_default_health 2,
+		new Creature called "Zombie Goliath"
+			with_default_damage 4
+			with_default_health 4
+			has_ability Reach
 	)
 
 	def createDeck(cardCount: Int = 20): Deck = {
